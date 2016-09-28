af7832f060b370d413d753ef17f8f5ae5659471e
Thomas Baker <ThomasHenr3@gmail.com>
2016-09-28 18:48:03 -0500

    Slimming
    took out most all of the extra stuff and got it to be 6 cols (except
    when screen is made really small)

--- demo/simple/simple.js
+++ demo/simple/simple.js
@@ -1,14 +1,29 @@
+angular.module("demo", ["ngRoute", "dndLists"])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/simple', {
+                templateUrl: 'simple/simple-frame.html',
+                controller: 'SimpleDemoController'
+            })
+            .otherwise({redirectTo: '/simple'});
+    });
+
+
 angular.module("demo").controller("SimpleDemoController", function($scope) {
 
     $scope.models = {
         selected: null,
-        lists: {"A": [], "B": []}
+        lists: {"A": [],"B": [],"C": [],"D": [],"E": [], "F": []}
     };
 
     // Generate initial model
     for (var i = 1; i <= 3; ++i) {
         $scope.models.lists.A.push({label: "Item A" + i});
         $scope.models.lists.B.push({label: "Item B" + i});
+        $scope.models.lists.C.push({label: "Item B" + i});
+        $scope.models.lists.D.push({label: "Item B" + i});
+        $scope.models.lists.E.push({label: "Item B" + i});
+        $scope.models.lists.F.push({label: "Item B" + i});
     }
 
     // Model to JSON for demo purpose
