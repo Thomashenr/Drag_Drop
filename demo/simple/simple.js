16e5f3c51c60b562047757ac00e9f064a68e9351
Thomas Baker <ThomasHenr3@gmail.com>
2016-09-28 21:04:46 -0500

    .

--- demo/simple/simple.js
+++ demo/simple/simple.js
@@ -8,22 +8,26 @@ angular.module("demo", ["ngRoute", "dndLists"])
             .otherwise({redirectTo: '/simple'});
     });
 
-
 angular.module("demo").controller("SimpleDemoController", function($scope) {
 
     $scope.models = {
         selected: null,
-        lists: {"A": [],"B": [],"C": [],"D": [],"E": [], "F": []}
+        lists: {"Quote Requested": [],
+                "In Process (Need More Info)": [],
+                "In Process (Delivered to Carrier)": [],
+                "In Process (Quote Delivered)": [],
+                "Policy Executed (Won)": [],
+                "Policy Declined (Lost/Dead)": []}
     };
 
-    // Generate initial model
+    // Fill with dummy info
     for (var i = 1; i <= 3; ++i) {
-        $scope.models.lists.A.push({label: "Item A" + i});
-        $scope.models.lists.B.push({label: "Item B" + i});
-        $scope.models.lists.C.push({label: "Item B" + i});
-        $scope.models.lists.D.push({label: "Item B" + i});
-        $scope.models.lists.E.push({label: "Item B" + i});
-        $scope.models.lists.F.push({label: "Item B" + i});
+        $scope.models.lists["Quote Requested"].push(                    {label: "Item A" + i});
+        $scope.models.lists["In Process (Need More Info)"].push(        {label: "Item B" + i});
+        $scope.models.lists["In Process (Delivered to Carrier)"].push(  {label: "Item B" + i});
+        $scope.models.lists["In Process (Quote Delivered)"].push(       {label: "Item B" + i});
+        $scope.models.lists["Policy Executed (Won)"].push(              {label: "Item B" + i});
+        $scope.models.lists["Policy Declined (Lost/Dead)"].push(        {label: "Item B" + i});
     }
 
     // Model to JSON for demo purpose
